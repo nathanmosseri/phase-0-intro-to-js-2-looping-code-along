a2b477c402c1aa660f066791617696d314fa2911
Nathan <namo678@me.com>
2022-07-23 19:43:35 -0400

    commit tree 32118fcada6ae49ea5a533203c57f71c8844ef34

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const writeCards = (people, eventType) => {
+    const messages = []
+    for (let i = 0; i < people.length; i++) {
+        messages.push(`Thank you, ${people[i]}, for the wonderful ${eventType} gift!`)
+        
+    }
+    return messages
+    
+}
+
+const countDown = () => {
+    let count = 11
+    while (count > 0) {
+        console.log(count -= 1)
+    }
+}
